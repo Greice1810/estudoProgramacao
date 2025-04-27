1c918ef8bd019a83a58404161f314b66e9dc7e02
Grécia1810 <ogreice824@gmail.com>
2025-04-27 20:04:32 -0300

    codigo conta salario

--- CodigosJava/ProjetoBanco/src/com/java/TestConta.java
+++ CodigosJava/ProjetoBanco/src/com/java/TestConta.java
@@ -3,31 +3,9 @@ package com.java;
 public class TestConta {
 
 	public static void main(String[] args) {
-		Conta  contaCorrente = new Conta();
-		Conta contaPoupanca = new Conta();
-		Conta contaInvest= new Conta();
-
-		contaCorrente.numero = 11;
-		contaCorrente.saldo = 20.5;
-		contaCorrente.cliente.nome = "greice";
-		contaCorrente.cliente.idade = 34;
-
-		contaPoupanca.numero = 22;
-		contaPoupanca.saldo = 30;
-		contaPoupanca.cliente.nome = "nicollas";
-		contaPoupanca.cliente.idade = 18;
-
-
-		contaInvest.numero = 33;
-		contaInvest.saldo = 40;
-		contaInvest.cliente.nome = "Welton";
-		contaInvest.cliente.idade = 40;
-
-		System.out.println(contaPoupanca.numero);
-		System.out.println(contaPoupanca.saldo);
-		System.out.println(contaPoupanca.cliente.nome);
-		System.out.println(contaPoupanca.cliente.idade);
-
-
+		 Conta cc = new Conta(1000, 50.0);
+		
+		 System.out.println(cc.recuperarSaldo());
 	}
 }
+	
